b298b58b421ac2249c308856e12326bead3c2ceb
YevhDr <evgesha.drozdova@gmail.com>
2021-09-17 21:09:14 +0300

    поправила стилі

--- js/caarousel.js
+++ js/caarousel.js
@@ -4,8 +4,12 @@
 d3.select("#next").on("click", function(){
     d3.select("#back").style("opacity", 1);
     let currentLeft = Math.abs(parseInt(d3.select(".slider").style("margin-left").replace("px", "")));
-    if(currentLeft < 1700) {
+    if(currentLeft < 2100) {
         d3.select(".slider").style("margin-left", -(currentLeft + 350)+ "px")
+        let new_left = currentLeft + 350;
+        if(new_left == 2100){
+            d3.select(this).style("opacity", 0.2);
+        }
     } else {
         d3.select(this).style("opacity", 0.2)
     }
@@ -16,7 +20,11 @@ d3.select("#back").on("click", function(){
     let currentLeft = parseInt(d3.select(".slider").style("margin-left").replace("px", ""));
     if(currentLeft < 0) {
         let abs_current = Math.abs(currentLeft);
+        let new_left = abs_current - 350;
         d3.select(".slider").style("margin-left", -(abs_current - 350)+ "px")
+        if(new_left == 0){
+            d3.select(this).style("opacity", 0.2);
+        }
     } else {
         d3.select(this).style("opacity", 0.2);
         d3.select("#next").style("opacity", 1)

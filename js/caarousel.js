b980d0754b51f7caeed720f73c05d9a21ea6645a
YevhDr <evgesha.drozdova@gmail.com>
2021-09-21 18:56:02 +0300

    поправила описи вагонів

--- js/caarousel.js
+++ js/caarousel.js
@@ -1,10 +1,10 @@
 d3.select("#next").on("click", function(){
     d3.select("#back").style("opacity", 1);
     let currentLeft = Math.abs(parseInt(d3.select(".slider").style("margin-left").replace("px", "")));
-    if(currentLeft < 2100) {
-        d3.select(".slider").style("margin-left", -(currentLeft + 350)+ "px")
-        let new_left = currentLeft + 350;
-        if(new_left == 2100){
+    if(currentLeft < 2400) {
+        d3.select(".slider").style("margin-left", -(currentLeft + 400)+ "px")
+        let new_left = currentLeft + 400;
+        if(new_left == 2400){
             d3.select(this).style("opacity", 0.2);
         }
     } else {
@@ -18,8 +18,8 @@ d3.select("#back").on("click", function(){
     let currentLeft = parseInt(d3.select(".slider").style("margin-left").replace("px", ""));
     if(currentLeft < 0) {
         let abs_current = Math.abs(currentLeft);
-        let new_left = abs_current - 350;
-        d3.select(".slider").style("margin-left", -(abs_current - 350)+ "px")
+        let new_left = abs_current - 400;
+        d3.select(".slider").style("margin-left", -(abs_current - 400)+ "px")
         if(new_left == 0){
             d3.select(this).style("opacity", 0.2);
         }
